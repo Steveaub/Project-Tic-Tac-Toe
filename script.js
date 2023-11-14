39cb852f14ef9f2654f4c74bb3de7e3027c90d63
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-11-13 20:20:10 -0500

    Initialize Gameboard Module with Basic Structure
    - Created the Gameboard module using an IIFE pattern.
    - Initialized an empty board array to represent the game state.
    - This sets up the foundational structure for the game's board
       management.

---
+++ script.js
@@ -0,0 +1,18 @@
+const Gameboard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => {
+     
+  };
+
+  const makeMove = (index, playerSymbol) => {
+    
+  };
+
+  const resetBoard = () => {
+    
+  };
+
+  return { getBoard, makeMove, resetBoard };
+})();
+
