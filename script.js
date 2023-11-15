d29994bf2d563f756dfae33a46e081042d2aa3bd
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-11-15 14:33:43 -0500

    Complete Basic Functionality of Gameboard Module
    - Implemented 'makeMove' method in Gameboard module. This method
      updates the gameboard state and includes validation to prevent
      overwriting existing moves.
    - Added 'getBoard' method for retrieving the current gameboard state.
    - Developed 'resetBoard' method using 'fill' to reset the gameboard for
      a new game.
    - Performed initial tests to ensure functionality of the new methods.

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const Gameboard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
 
-  const getBoard = () => {
-     
-  };
+  const getBoard = () => board;
 
   const makeMove = (index, playerSymbol) => {
-    
+ if (board[index] === "") {
+  board[index] = playerSymbol;
+
+ } else {console.log("Invalid choice please try again")}
+
   };
 
   const resetBoard = () => {
-    
+    board.fill("");
   };
 
   return { getBoard, makeMove, resetBoard };
 })();
+
+
+console.log(Gameboard.getBoard())
 
