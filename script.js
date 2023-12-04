21207f1f6c0898a5b857de8ccee31b1ebfa9f1b0
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-12-03 23:26:01 -0500

    Improve Game UI and Styling
    - Enhanced the Tic-Tac-Toe game board with CSS grid layout for a more
      structured and visually appealing interface.
    - Styled the game cells for better user interaction, including hover
     effects
      and distinct X/O representations.
    - Updated game status and restart button styling for improved visibility
      and accessibility.
    - Added responsive design considerations to ensure the game is playable
      across various devices and screen sizes.
    - Implemented animations and transitions for game moves and winning
      scenarios to enrich the user experience.
    
    These updates significantly improve the overall look and feel of the
    game, making it not only functional but also enjoyable to play.

--- script.js
+++ script.js
@@ -4,99 +4,116 @@ const Gameboard = (() => {
   const getBoard = () => board;
 
   const makeMove = (index, playerSymbol) => {
- if (board[index] === "") {
-  board[index] = playerSymbol;
-
- } else {console.log("Invalid choice please try again")}
-return playerSymbol
+      if (board[index] === "") {
+          board[index] = playerSymbol;
+          return true; // Move was successful
+      } else {
+          console.log("Invalid choice, please try again");
+          return false; // Move was unsuccessful
+      }
   };
 
   const resetBoard = () => {
-    board.fill("");
+      board.fill("");
   };
 
   return { getBoard, makeMove, resetBoard };
 })();
 
 function createPlayer(name, symbol) {
-  
   return {
       getName: () => name,
       getSymbol: () => symbol,
-      
   };
 }
 
-
-// Usage
-const player1 = createPlayer("Alice", "X");
-const player2 = createPlayer("Bob", "O");
-
-console.log(player1.getName());
-
 const GameController = (() => {
-  let currentPlayer;
+  const player1 = createPlayer("Alice", "X");
+  const player2 = createPlayer("Bob", "O");
+  let currentPlayer = player1;
+
+  const updateGameStatus = (message) => {
+      const gameStatus = document.getElementById("gameStatus");
+      gameStatus.textContent = message;
+  };
 
   const startGame = () => {
-    Gameboard.resetBoard();
-    currentPlayer = player1; 
+      Gameboard.resetBoard();
+      currentPlayer = player1;
+      updateGameStatus("Player 1's turn");
   };
-  const makeMove = (index) => {
-    if (Gameboard.makeMove(index, currentPlayer.getSymbol())) {
-        if (checkForWin()) {
-            // Handle win
-        } else if (checkForDraw()) {
-            // Handle draw
-        } else {
-            switchTurns();
-        }
-    }
-};
-const switchTurns = () => {
-  currentPlayer = (currentPlayer === player1) ? player2 : player1;
-  
-};
 
-const checkForWin = () => {
-  const symbol = currentPlayer.getSymbol();
-  
-  for (let i = 0; i < 3; i++) {
-      if (board[i * 3] === symbol && board[i * 3 + 1] === symbol && board[i * 3 + 2] === symbol) {
-          return true;
+  const switchTurns = () => {
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+      updateGameStatus(`Player ${currentPlayer.getName()}'s turn`);
+  };
+
+  const checkForWin = () => {
+      const symbol = currentPlayer.getSymbol();
+      const b = Gameboard.getBoard();
+
+      for (let i = 0; i < 3; i++) {
+          if (b[i * 3] === symbol && b[i * 3 + 1] === symbol && b[i * 3 + 2] === symbol) {
+              return true;
+          }
+          if (b[i] === symbol && b[i + 3] === symbol && b[i + 6] === symbol) {
+              return true;
+          }
       }
-  }
- 
 
-    if (board[0] === symbol && board[4] === symbol && board[8] === symbol) {
-      return true;
-  }
-  if (board[2] === symbol && board[4] === symbol && board[6] === symbol) {
-      return true;
-  }
+      if (b[0] === symbol && b[4] === symbol && b[8] === symbol) return true;
+      if (b[2] === symbol && b[4] === symbol && b[6] === symbol) return true;
 
-  return false; 
+      return false;
+  };
 
-};
+  const checkForDraw = () => {
+      return Gameboard.getBoard().every(cell => cell !== "") && !checkForWin();
+  };
 
-
-const checkForDraw = () => {
-  const isBoardFull = board.every(cell => cell !== "");
-
-  const noWinner = !checkForWin();
-
-  return isBoardFull && noWinner;
-};
-
+  const makeMove = (index) => {
+      if (Gameboard.makeMove(index, currentPlayer.getSymbol())) {
+          if (checkForWin()) {
+              updateGameStatus(`Player ${currentPlayer.getName()} wins!`);
+          } else if (checkForDraw()) {
+              updateGameStatus('Game is a draw!');
+          } else {
+              switchTurns();
+          }
+      }
+  };
 
   const resetGame = () => {
-    Gameboard.resetBoard();
-    startGame()
+      Gameboard.resetBoard();
+      startGame();
   };
 
-  return { startGame, switchTurns, checkForWin, checkForDraw, resetGame };
+  return { startGame, makeMove, switchTurns, checkForWin, checkForDraw, resetGame };
 })();
 
+// UI Interaction
+document.addEventListener("DOMContentLoaded", () => {
+  const cells = document.querySelectorAll(".cell");
+  const restartButton = document.getElementById("restartButton");
 
-console.log(Gameboard.getBoard())
-console.log(Gameboard.makeMove(0, "X"));
-console.log(Gameboard.getBoard());
+  GameController.startGame();
+
+  cells.forEach(cell => {
+      cell.addEventListener("click", () => {
+          GameController.makeMove(parseInt(cell.getAttribute('data-index')));
+          updateBoardUI();
+      });
+  });
+
+  restartButton.addEventListener("click", () => {
+      GameController.resetGame();
+      updateBoardUI();
+  });
+
+  function updateBoardUI() {
+      const board = Gameboard.getBoard();
+      board.forEach((cell, index) => {
+          cells[index].textContent = cell;
+      });
+  }
+});
