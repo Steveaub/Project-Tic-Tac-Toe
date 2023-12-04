51533fc67a95b6b2ffc08aee9258fa8ca9157fd4
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-12-03 19:22:02 -0500

    Add Win and Draw Logic to Game
    - Implemented checkForWin function to evaluate winning conditions
      in the GameController. It checks horizontal, vertical, and
      diagonal lines for a win based on the current player's moves.
    
    - Developed checkForDraw function in GameController to determine
      if the game has ended in a draw. This function checks if the
      board is full and there's no winner.
    
    - These additions enhance the game's core logic, allowing for
      complete game-play functionality including determining the
      outcome of each game.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const Gameboard = (() => {
   board[index] = playerSymbol;
 
  } else {console.log("Invalid choice please try again")}
-
+return playerSymbol
   };
 
   const resetBoard = () => {
@@ -18,6 +18,85 @@ const Gameboard = (() => {
   return { getBoard, makeMove, resetBoard };
 })();
 
+function createPlayer(name, symbol) {
+  
+  return {
+      getName: () => name,
+      getSymbol: () => symbol,
+      
+  };
+}
+
+
+// Usage
+const player1 = createPlayer("Alice", "X");
+const player2 = createPlayer("Bob", "O");
+
+console.log(player1.getName());
+
+const GameController = (() => {
+  let currentPlayer;
+
+  const startGame = () => {
+    Gameboard.resetBoard();
+    currentPlayer = player1; 
+  };
+  const makeMove = (index) => {
+    if (Gameboard.makeMove(index, currentPlayer.getSymbol())) {
+        if (checkForWin()) {
+            // Handle win
+        } else if (checkForDraw()) {
+            // Handle draw
+        } else {
+            switchTurns();
+        }
+    }
+};
+const switchTurns = () => {
+  currentPlayer = (currentPlayer === player1) ? player2 : player1;
+  
+};
+
+const checkForWin = () => {
+  const symbol = currentPlayer.getSymbol();
+  
+  for (let i = 0; i < 3; i++) {
+      if (board[i * 3] === symbol && board[i * 3 + 1] === symbol && board[i * 3 + 2] === symbol) {
+          return true;
+      }
+  }
+ 
+
+    if (board[0] === symbol && board[4] === symbol && board[8] === symbol) {
+      return true;
+  }
+  if (board[2] === symbol && board[4] === symbol && board[6] === symbol) {
+      return true;
+  }
+
+  return false; 
+
+};
+
+
+const checkForDraw = () => {
+  const isBoardFull = board.every(cell => cell !== "");
+
+  const noWinner = !checkForWin();
+
+  return isBoardFull && noWinner;
+};
+
+
+  const resetGame = () => {
+    Gameboard.resetBoard();
+    startGame()
+  };
+
+  return { startGame, switchTurns, checkForWin, checkForDraw, resetGame };
+})();
+
 
 console.log(Gameboard.getBoard())
-
+console.log(Gameboard.makeMove(0, "X"));
+console.log(Gameboard.getBoard());
